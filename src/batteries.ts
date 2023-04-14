eca137e9ca1347756b55abe95ebce295ce776214
Neel Yadav <mail@neelyadav.com>
2023-04-14 15:32:03 -0500

    Simplify setup and checks on activation

--- src/batteries.ts
+++ src/batteries.ts
@@ -7,44 +7,28 @@ export let standard = nova.path.join(dir, "standard.yaml");
 export let linter   = nova.path.join(bin, "stylelint");
 
 export async function kickstart() {
-    function check() {
-        return nova.fs.access(linter, nova.fs.X_OK);
-    }
+    const check = () => nova.fs.access(linter, nova.fs.X_OK);
 
-    function unpack() {
+    if ( ! check() ) {
         console.log("Waiting for initial extension installation to complete...");
 
-        return runProc(dir, "/usr/bin/env", "npm", "ci")
-            .then(npm => console.log(npm))
-            .then(success => console.log("...installed successfully."))
-            .then(resolve => true)
-            .catch(err => {
-                console.error(err);
-                return false;
-            });
-    }
+        await runProc(dir, "/usr/bin/env", "npm", "ci", "--legacy-peer-deps=false")
+            .then(npm => {
+                console.log(npm);
+                console.log("...installed successfully.");
+            })
+            .catch(e => console.error(`Extension installation failed: ${e}`));
 
-    async function init() {
-        for ( let n = 1; n <= 3; n++ ) {
-            if ( await unpack() ) {
-                return true;
-            } else {
-                console.warn(`Extension installation failed on attempt ${n}.`);
-            }
+        if ( ! check() ) {
+            // @TODO: factor out all user feedback
+            const title = "Activation Error";
+            const msg = "Extension dependency installation was unsuccessful.";
+            const hint = "Make sure Node.js and npm are installed and that Nova is able to access both CLIs through your $PATH environment variable.";
+
+            alert(`${title}\n\n${msg}\n\n${hint}`, "Report");
+            return false;
         }
-
-        return check();
     }
 
-    function fatal() {
-        // @TODO: factor out all user feedback
-        const title = "Activation Error";
-        const msg = "Extension dependency installation aborted after 3 failed attempts.";
-        const hint = "Make sure Node.js and npm are installed and that Nova is able access both CLIs through your $PATH environment variable.";
-
-        alert(`${title}\n\n${msg}\n\n${hint}`, "Report");
-        return false;
-    }
-
-    return check() || await init() || fatal();
+    return true;
 }
